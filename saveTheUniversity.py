087cbb996a778a3c7d9dceeac7ee0425c528a275
Julius J. Clores <juliusclores17@gmail.com>
2026-05-04 13:25:03 +0800

    Update saveTheUniversity.py

--- saveTheUniversity.py
+++ saveTheUniversity.py
@@ -16,12 +16,12 @@ bigFont = pygame.font.SysFont("Arial", 60)
 
 clock = pygame.time.Clock()
 
-background = pygame.image.load("pictures/background.jpg")
+background = pygame.image.load("images/newBackground.png")
 background = pygame.transform.scale(background, (width, height))
 
-objectImg = pygame.transform.scale(pygame.image.load("log-removebg-preview.png"), (50, 50))
+objectImg = pygame.transform.scale(pygame.image.load("images/UddLogo.png"), (50, 50))
 
-playerImg = pygame.transform.scale(pygame.image.load("image/Chibi_char_pixel2.png"), (120, 100))
+playerImg = pygame.transform.scale(pygame.image.load("images/Chibi_char.png"), (120, 100))
 
 pygame.mixer.music.load("sounds/bgMusic.mp3")
 pygame.mixer.music.set_volume(0.2)
@@ -63,7 +63,7 @@ started = False
 
 playedLoseSound = False
 playedWinSound = False
-#Marquez, Porbile, Dela cruz
+
 while True:
     screen.blit(background, (0, 0))
 
@@ -86,16 +86,14 @@ while True:
                 playedWinSound = False
                 pygame.mixer.music.play(-1, 0.0, 3000)
 
-
     if not started:
         startKey = ""
 
-        startImg = pygame.transform.scale(pygame.image.load("images/=====.png"), (400, 200))
+        startImg = pygame.transform.scale(pygame.image.load("images/introPic.png"), (400, 200))
         screen.blit(startImg, (100, 50))
 
         startButton = pygame.transform.scale(pygame.image.load("images/startButton.png"), (250, 200))
         screen.blit(startButton, (180, 550))
-
 
     elif not gameOver and not won:
         spikes = pygame.transform.scale(pygame.image.load("images/spikes.png"), (700, 500))
@@ -111,20 +109,23 @@ while True:
         player.x = max(0, min(width - player.width, player.x))
 
         logo.y += speed
-#Clores, Danan
+    
         if logo.colliderect(player):
             catchSound.play()
             score += 1
             logo.topleft = (random.randint(0, width-50), 0)
             speed += 0.3
 
-
-        if logo.top > height:
+        if logo.top > height: 
             gameOver = True
 
         if score == winScore:
             won = True
-
+        
+        spikes =  pygame.Rect(0, 780, 700, 500)
+        if logo.colliderect(spikes):
+            gameOver = True
+    
         screen.blit(playerImg, player)
         screen.blit(objectImg, logo)
         screen.blit(font.render(f"Score: {score}/50", True, "black"), (10, 10))
@@ -134,9 +135,8 @@ while True:
             pygame.mixer.music.stop()
             loseSound.play()
             playedLoseSound = True
-
-
-        box = pygame.transform.scale(pygame.image.load("pictures/box.png"), (550, 800))
+       
+        box = pygame.transform.scale(pygame.image.load("images/box.png"), (550, 800))
         screen.blit(box, (40, 80))
 
         screen.blit(bigFont.render("GAME OVER", True, "red"), (160, 200))
@@ -157,4 +157,3 @@ while True:
 
     pygame.display.flip()
     clock.tick(60)
-#De guzman, Lomboy
