1368c997b2543c7698cc10744cf5df0e3f321014
Julius J. Clores <juliusclores17@gmail.com>
2026-05-06 02:15:10 +0800

    Update saveTheUniversity.py

--- saveTheUniversity.py
+++ saveTheUniversity.py
@@ -11,17 +11,17 @@ height = 800
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Save the University")
 
-font = pygame.font.SysFont("Arial", 30)
-bigFont = pygame.font.SysFont("Arial", 60)
+smallPixelFont = pygame.font.Font("font/pixelatedElegance.ttf", 20)
+bigPixelFont = pygame.font.Font("font/pixelatedElegance.ttf", 50)
 
 clock = pygame.time.Clock()
 
-background = pygame.image.load("images/newBackground.png")
+background = pygame.image.load("pictures/newBackground.png")
 background = pygame.transform.scale(background, (width, height))
 
-objectImg = pygame.transform.scale(pygame.image.load("images/UddLogo.png"), (50, 50))
+objectImg = pygame.transform.scale(pygame.image.load("pictures/UddLogo.png"), (50, 50))
 
-playerImg = pygame.transform.scale(pygame.image.load("images/Chibi_char.png"), (120, 100))
+playerImg = pygame.transform.scale(pygame.image.load("pictures/Chibi char.png"), (120, 100))
 
 pygame.mixer.music.load("sounds/bgMusic.mp3")
 pygame.mixer.music.set_volume(0.2)
@@ -40,7 +40,9 @@ dares = [
     "Sing a song",
     "Do 20 push-ups",
     "Try to lick your elbow",
-    "Kiss your classmate"
+    "Kiss your classmate",
+    "Borrow money from stranger",
+    "Confess to your crush"
 ]
 
 currentDare = random.choice(dares)
@@ -50,9 +52,7 @@ def resetGame():
     currentDare = random.choice(dares)
     return (
         pygame.Rect(250, 700, 120, 60),
-        pygame.Rect(random.randint(0, width-50), 0, 50, 50),
-        0,
-        5
+        pygame.Rect(random.randint(0, width-50), 0, 50, 50),0, 5
     )
 
 player, logo, score, speed = resetGame()
@@ -89,14 +89,14 @@ while True:
     if not started:
         startKey = ""
 
-        startImg = pygame.transform.scale(pygame.image.load("images/introPic.png"), (400, 200))
+        startImg = pygame.transform.scale(pygame.image.load("pictures/introPic.png"), (400, 200))
         screen.blit(startImg, (100, 50))
 
-        startButton = pygame.transform.scale(pygame.image.load("images/startButton.png"), (250, 200))
+        startButton = pygame.transform.scale(pygame.image.load("pictures/startButton.png"), (250, 200))
         screen.blit(startButton, (180, 550))
 
     elif not gameOver and not won:
-        spikes = pygame.transform.scale(pygame.image.load("images/spikes.png"), (700, 500))
+        spikes = pygame.transform.scale(pygame.image.load("pictures/spikes.png"), (700, 500))
         screen.blit(spikes, (0, 545))
         
         keys = pygame.key.get_pressed()
@@ -125,24 +125,24 @@ while True:
         spikes =  pygame.Rect(0, 780, 700, 500)
         if logo.colliderect(spikes):
             gameOver = True
-    
+
         screen.blit(playerImg, player)
         screen.blit(objectImg, logo)
-        screen.blit(font.render(f"Score: {score}/50", True, "black"), (10, 10))
+        screen.blit(smallPixelFont.render(f"Score: {score}/50", True, "black"), (10, 10))
 
     elif gameOver:
         if not playedLoseSound:
             pygame.mixer.music.stop()
             loseSound.play()
             playedLoseSound = True
-       
-        box = pygame.transform.scale(pygame.image.load("images/box.png"), (550, 800))
-        screen.blit(box, (40, 80))
 
-        screen.blit(bigFont.render("GAME OVER", True, "red"), (160, 200))
-        screen.blit(font.render("Your Dare:", True, "black"), (130, 270))
-        screen.blit(font.render(currentDare, True, "black"), (150, 320))
-        screen.blit(font.render("Press SPACE to Restart", True, "black"), (190, 550))
+        box = pygame.transform.scale(pygame.image.load("pictures/box.png"), (550, 600))
+        screen.blit(box, (30, 80))
+
+        screen.blit(bigPixelFont.render("GAME OVER", False, "red"), (130, 150))
+        screen.blit(smallPixelFont.render("Your Dare:", False, "black"), (70, 230))
+        screen.blit(smallPixelFont.render(currentDare, False, "black"), (100, 280))
+        screen.blit(smallPixelFont.render("Press SPACE to Restart", False, "black"), (160, 550))
         
     elif won:
         if not playedWinSound:
@@ -150,10 +150,12 @@ while True:
             winSound.play()
             playedWinSound = True
 
-        screen.blit(bigFont.render("YOU WIN!", True, "blue"), (180, 250))
-        screen.blit(font.render("You caught all 50!", True, "black"), (180, 320))
-        screen.blit(font.render("Press SPACE to Play Again", True, "black"), (130, 400))
-        screen.blit(box, (100, 100))
+        box = pygame.transform.scale(pygame.image.load("pictures/box.png"), (550, 600))
+        screen.blit(box, (30, 80))
+
+        screen.blit(bigPixelFont.render("YOU WIN!", False, "Green"), (170, 150))
+        screen.blit(smallPixelFont.render("You caught all 50!", False, "black"), (180, 280))
+        screen.blit(smallPixelFont.render("Press SPACE to Play Again", False, "black"), (130, 550))
 
     pygame.display.flip()
     clock.tick(60)
